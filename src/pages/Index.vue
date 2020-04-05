495af6d416642113376bc82ca41646597e98f0ef
abenezerYakobSweetooia <60691579+abenezerYakobSweetooia@users.noreply.github.com>
2020-04-05 23:47:04 +0200

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout :sidebar="false">
     <div class="content">
-      <h1> Corona related information for Ethiopian medical teams</h1>
+      <h2> Corona related information for Ethiopian medical teams</h2>
       <h4> {{ this.description }}</h4>
       <nav>
         <!-- To use other icons here, you need to import them in the Shortcut component -->
